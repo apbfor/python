dd0d28c56b659bdc6f680116a9544bfe0e4d412f
apbfor <apbforgame@gmail.com>
2019-11-30 00:18:47 +0300

    pz4

--- pz4/pz4.py
+++ pz4/pz4.py
@@ -1,10 +1,42 @@
+# -*- coding: utf-8 -*-
 """
-В этом модуле реализована функция,
-позволяющая пользователю посмотреть
-список файлов и папок, находящихся
-по пути, указанном этим пользователем
+В этом модуле реализованы функции,
+для выполенения заданий из ПЗ4
 """
 import os
+import random
+
+
+def calc(var1, var2, operation):
+    """
+    This is calculator .
+
+    Args:
+        var1: First int arg.
+        var2: Second int arg.
+        operation: *,/,-,+ are available
+
+    Returns:
+        int value of calculation
+
+    Raises:
+        ValueError: Raises an exception while args
+        or operation is not correct.
+    """
+
+    operations = {
+        '+': int.__add__,
+        '*': int.__mul__,
+        '-': int.__sub__,
+        '/': int.__truediv__
+    }
+
+    if not isinstance(var1, int) or not isinstance(var2, int):
+        raise ValueError("Введенные числа не являются целыми")
+
+    if operation not in operations:
+        raise ValueError("Такой операции не существует")
+    return operations[operation](var1, var2)
 
 
 def listdir():
@@ -35,6 +67,58 @@ def listdir():
         except FileNotFoundError as error:
             print("Не существует", error)
             return
+
+
+class Guesser:
+    """Play a 'guess a number' game"""
+
+    def __init__(self):
+        """Init a random number for guesser"""
+        self.rnd = random.randint(0, 100)
+
+    def check(self, guess):
+        """Check if guess was right
+        Args:
+            guess: str from user that guess a number
+
+        Returns:
+            0 if correct
+            1 if guess > self.rnd
+            -1 if guess < self.rnd
+
+        Raises:
+            ValueError: if guess cant be converted to int.
+        """
+        if guess == self.rnd:
+            return 0
+        if guess > self.rnd:
+            return 1
+        return -1
+
+    def play(self):
+        """Start a guess game"""
+        while True:
+            answer = input('Угадайте число: ')
+            if answer == 'exit':
+                break
+            if not isinstance(answer, int):
+                raise ValueError("Вы ввели не число")
+            check = self.check(answer)
+            if not check:
+                print('\nУспех! Верно угадано число {0}'.format(self.rnd))
+                break
+            elif check == 1:
+                print('Бери ниже')
+            elif check == -1:
+                print('Бери выше')
+            else:
+                print('Что-то сломалось')
+
+
+def run_guesser():
+    """Run guesser"""
+    guesser = Guesser()
+    guesser.play()
 
 
 if __name__ == '__main__':
--- pz4/test_pz.py
+++ pz4/test_pz.py
@@ -1,8 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Модуль с тестами для ПЗ4
+"""
 import unittest
-from calc import calc
+from unittest.mock import patch
+from pz4 import calc
+from pz4 import Guesser
 
 
 class TestCalc(unittest.TestCase):
+    """
+    Тесты для калькулятора
+    """
     def test_calc_add(self):
         a = [10, 20, 40, -10]
         b = 20
@@ -22,7 +31,7 @@ class TestCalc(unittest.TestCase):
     def test_calc_div(self):
         a = [10, 20, 40, -10]
         b = 20
-        should_ans = [10/20, 1, 2, -10/20]
+        should_ans = [0.5, 1, 2, -0.5]
         for i in range(len(a)):
             res = calc(a[i], b, '/')
             self.assertEqual(res, should_ans[i])
@@ -34,6 +43,67 @@ class TestCalc(unittest.TestCase):
         for i in range(len(a)):
             res = calc(a[i], b, '-')
             self.assertEqual(res, should_ans[i])
+
+    def test_calc_not_integer(self):
+        a = 'a'
+        b = 10
+        op = '+'
+        with self.assertRaises(ValueError):
+            res = calc(a, b, op)
+
+    def test_calc_invalid_operation(self):
+        a = 5
+        b = 15
+        op = 's'
+        with self.assertRaises(ValueError):
+            res = calc(a, b, op)
+
+
+class TestGuesser(unittest.TestCase):
+    """Класс, содержащий тесты для угадайки"""
+    def test_check(self):
+        """Основной тест"""
+        rnd = 15
+        with patch('random.randint', return_value=rnd):
+            guesser = Guesser()
+        guess = 15
+        should_ans = 0
+        self.assertEqual(guesser.check(guess), should_ans)
+
+    def test_map(self):
+        """Те же тесты, но в словарике"""
+        rnd = [5, 10, 15, 30, 45, 60, 100]
+        guess = [5, 10, 15, 30, 45, 60, 100]
+        should_ans = 0
+        for ind, item in enumerate(rnd):
+            with patch('random.randint', return_value=item):
+                guesser = Guesser()
+            self.assertEqual(guesser.check(guess[ind]), should_ans)
+
+    def test_return(self):
+        rnd = 50
+        with patch('random.randint', return_value=rnd):
+            guesser = Guesser()
+        guess = 30
+        should_ans = -1
+        self.assertEqual(guesser.check(guess), should_ans)
+        guess = 60
+        should_ans = 1
+        self.assertEqual(guesser.check(guess), should_ans)
+
+    def test_play(self):
+        """change user input"""
+        with patch('random.randint', return_value=15):
+            with patch('builtins.input', return_value=15):
+                guess = Guesser()
+                self.assertEqual(guess.play(), None)
+
+    def test_play_error(self):
+        """check error"""
+        with patch('builtins.input', return_value='abcdefg'):
+            with self.assertRaises(ValueError):
+                guess = Guesser()
+                guess.play()
 
 
 if __name__ == '__main__':
